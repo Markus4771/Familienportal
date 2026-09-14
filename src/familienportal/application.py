a75db94a79c7319c9f8464b9e21bf832d481329d
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-09-14 17:10:18 +0200

    Register Gramps Web integration

--- src/familienportal/application.py
+++ src/familienportal/application.py
@@ -1,7 +1,9 @@
+from familienportal.gramps_web import router as gramps_router
 from familienportal.main import app
 from familienportal.role_mfa_policy import RoleMfaPolicyMiddleware
 from familienportal.security_http import CsrfOriginMiddleware, SecurityHeadersMiddleware
 
+app.include_router(gramps_router)
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(CsrfOriginMiddleware)
 app.add_middleware(RoleMfaPolicyMiddleware)
