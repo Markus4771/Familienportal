4154a37a185d235f06fae474a17a4442c216a7ad
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-09-15 08:55:08 +0200

    Register genealogy privacy administration

--- src/familienportal/application.py
+++ src/familienportal/application.py
@@ -2,6 +2,7 @@ from familienportal.extensions import BUILTIN_MODULES
 from familienportal.genealogy_document_proxy import router as genealogy_document_proxy_router
 from familienportal.genealogy_documents_web import router as genealogy_documents_router
 from familienportal.genealogy_picker_web import router as genealogy_picker_router
+from familienportal.genealogy_privacy_web import router as genealogy_privacy_router
 from familienportal.gramps_api import router as gramps_api_router
 from familienportal.gramps_extra_web import router as gramps_extra_router
 from familienportal.gramps_person_web import router as gramps_person_router
@@ -22,6 +23,7 @@ app.include_router(gramps_person_router)
 app.include_router(genealogy_documents_router)
 app.include_router(genealogy_picker_router)
 app.include_router(genealogy_document_proxy_router)
+app.include_router(genealogy_privacy_router)
 app.include_router(paperless_router)
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(CsrfOriginMiddleware)
