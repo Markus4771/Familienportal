411d1ef9e53f1aff4995f89272b7c4d053e170c9
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-09-15 08:12:05 +0200

    Register genealogy document picker routes

--- src/familienportal/application.py
+++ src/familienportal/application.py
@@ -1,5 +1,6 @@
 from familienportal.extensions import BUILTIN_MODULES
 from familienportal.genealogy_documents_web import router as genealogy_documents_router
+from familienportal.genealogy_picker_web import router as genealogy_picker_router
 from familienportal.gramps_api import router as gramps_api_router
 from familienportal.gramps_extra_web import router as gramps_extra_router
 from familienportal.gramps_person_web import router as gramps_person_router
@@ -17,6 +18,7 @@ app.include_router(gramps_api_router)
 app.include_router(gramps_transfer_router)
 app.include_router(gramps_person_router)
 app.include_router(genealogy_documents_router)
+app.include_router(genealogy_picker_router)
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(CsrfOriginMiddleware)
 app.add_middleware(RoleMfaPolicyMiddleware)
